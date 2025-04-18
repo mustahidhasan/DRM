5959c41aeafc9b3de7ca67235910a90c37e43e3c
Mustahid Hasan <mustahidhasan9@gmail.com>
2025-04-18 08:46:45 +0600

    save

--- drm/drm/settings.py
+++ drm/drm/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "user",
-    "document",
     # "corsheaders",
 ]
 JAZZMIN_SETTINGS = {
--- drm/drm/urls.py
+++ drm/drm/urls.py
@@ -23,6 +23,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("user.urls")),
-    path("store/", include("document.urls")),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
